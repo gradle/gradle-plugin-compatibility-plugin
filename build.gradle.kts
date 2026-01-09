c10a131c91e01da25afe1d56a69ec53d60d620ad
Mikhail Lopatkin <mlopatkin@gradle.com>
2026-01-09 21:00:48 +0100

    Clean up the build script a bit

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,14 +16,18 @@
 
 import net.ltgt.gradle.errorprone.errorprone
 import net.ltgt.gradle.nullaway.nullaway
+import org.gradle.kotlin.dsl.invoke
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     `java-gradle-plugin`
+    `jvm-test-suite`
     `kotlin-dsl`
     `maven-publish`
-    `jvm-test-suite`
     checkstyle
+
     id("net.ltgt.errorprone") version "4.3.0"
     id("net.ltgt.nullaway") version "2.3.0"
 }
@@ -42,22 +46,10 @@ dependencies {
     //  - ErrorProne 2.42.0+ required JDK 21
     errorprone("com.google.errorprone:error_prone_core:2.42.0")
     errorprone("com.uber.nullaway:nullaway:0.12.14")
-
-    testImplementation("com.fasterxml.jackson.core:jackson-databind:2.20.0") {
-        because("Needed for parsing the Gradle releases metadata JSON")
-    }
-
-    testImplementation(platform("org.junit:junit-bom:6.0.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter-api")
-    testImplementation("org.junit.jupiter:junit-jupiter-params")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-
-    testImplementation("org.assertj:assertj-core:3.25.3")
 }
 
 kotlin {
-    // Also sets the Java toolchain
+    // Set up the JDK used to compile Java and Kotlin code.
     jvmToolchain(17)
 }
 
@@ -68,6 +60,7 @@ gradlePlugin {
             implementationClass = "org.gradle.plugin.compatibility.internal.CompatibilityPlugin"
         }
     }
+    // TODO(mlopatkin) Apply the plugin and define its compatibility.
 }
 
 val java8Launcher = javaToolchains.launcherFor {
@@ -76,17 +69,10 @@ val java8Launcher = javaToolchains.launcherFor {
 
 testing {
     suites {
-        val test by getting(JvmTestSuite::class) {
-            useJUnitJupiter()
-        }
-
-        val integTests by registering(JvmTestSuite::class) {
+        withType<JvmTestSuite>().configureEach {
             useJUnitJupiter()
 
             dependencies {
-                implementation(project())
-                implementation(gradleTestKit())
-                implementation("com.fasterxml.jackson.core:jackson-databind:2.20.0")
                 implementation(platform("org.junit:junit-bom:6.0.0"))
                 implementation("org.junit.jupiter:junit-jupiter-api")
                 implementation("org.junit.jupiter:junit-jupiter-params")
@@ -94,11 +80,26 @@ testing {
                 runtimeOnly("org.junit.platform:junit-platform-launcher")
                 implementation("org.assertj:assertj-core:3.25.3")
             }
+        }
+
+        named<JvmTestSuite>("test") {
+            dependencies {
+                implementation("com.fasterxml.jackson.core:jackson-databind:2.20.0")  {
+                    because("Needed for parsing the Gradle releases metadata JSON")
+                }
+            }
+        }
+
+        val integTests by registering(JvmTestSuite::class) {
+            dependencies {
+                implementation(project())
+                implementation(gradleTestKit())
+            }
 
             targets {
                 all {
                     testTask.configure {
-                        val pluginMetadata = tasks.named("pluginUnderTestMetadata")
+                        val pluginMetadata = tasks.pluginUnderTestMetadata
                         dependsOn(pluginMetadata)
                         classpath += files(pluginMetadata)
                     }
@@ -108,18 +109,20 @@ testing {
                 register("java8IntegTest") {
                     testTask.configure {
                         systemProperty("java8Home", java8Launcher.get().metadata.installationPath.asFile.absolutePath)
-                        val pluginMetadata = tasks.named("pluginUnderTestMetadata")
-                        dependsOn(pluginMetadata)
-                        classpath += files(pluginMetadata)
                     }
                 }
             }
+        }
+
+        tasks.check {
+            dependsOn(integTests)
         }
     }
 }
 
 tasks {
     compileJava {
+        // Compile production code to Java 8 bytecode with Java 8 APIs
         options.release = 8
     }
 
@@ -135,15 +138,15 @@ tasks {
     }
 
     withType<KotlinCompile>().configureEach {
+        // I don't understand why, but setting this config in `kotlin` block doesn't work, compilation on Gradle 7.4 fails
+        // with an error in Protobuf metadata parsing.
         compilerOptions {
-            languageVersion.set(org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_1_8)
-            apiVersion.set(org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_1_8)
-            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_1_8)
+            // Compile production code to Kotlin 1.8 to ensure compatibility with older Gradle versions, like 7.4
+            languageVersion = KotlinVersion.KOTLIN_1_8
+            apiVersion = KotlinVersion.KOTLIN_1_8
+            // Compile production code to Java 8 bytecode
+            jvmTarget = JvmTarget.JVM_1_8
         }
-    }
-
-    check {
-        dependsOn(testing.suites.named("integTests"))
     }
 
     register("checkstyle") {
